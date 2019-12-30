83773e3d66ad409ffa924756e256d3be24f23702
Tinus Mørch Abell <tinus.abell@jp.dk>
2019-12-30 10:30:51 +0100

    Init

---
+++ fish/config.fish
@@ -0,0 +1,5 @@
+alias vpn_connect='sudo vpnc JPPOL --local-port 1024'
+alias vpn_disconnect='sudo pkill vpnc'
+alias nuget='mono /usr/local/bin/nuget.exe'
+
+export DOTNET_CLI_TELEMETRY_OPTOUT=true
---
+++ fish/functions/fish_prompt.fish
@@ -0,0 +1,1 @@
+/home/tinux/.local/share/omf/themes/bobthefish/fish_prompt.fish
